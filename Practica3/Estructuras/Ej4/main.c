bd0a23a22d778aa3d92ecd0e9695144fddf20bcb
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-04-11 19:01:34 -0300

    Terminé ejercicio 4 de estructuras

--- Practica3/Estructuras/Ej4/main.c
+++ Practica3/Estructuras/Ej4/main.c
@@ -8,22 +8,39 @@ typedef struct{
 typedef struct{
     Carta cartas[50];
 }Mazo;
+void inicializar_mazo_ordenado(Mazo *m);
 void barajar_mazo(Mazo *m);
 Carta devolver_carta(Mazo *m);
 void imprimir_carta(Carta *c);
 int main()
 {
+    Mazo mazo;
+    inicializar_mazo_ordenado(&mazo);
+    int i;
+    for(i=0;i<50;i++){
+        printf("%d-",i+1);
+        imprimir_carta(&mazo.cartas[i]);
+    }
+    printf("\n");
+    barajar_mazo(&mazo);
+    for(i=0;i<50;i++){
+        printf("%d-",i+1);
+        imprimir_carta(&mazo.cartas[i]);
+    }
 
     return 0;
 }
 void inicializar_mazo_ordenado(Mazo *m){
-    int i=0,j=0,k=0;
-    Carta aux;
-    aux.palo=4;
-    aux.valor=0;
-    for(;i<4;i++){
-        for(;j<12,j++){
-
+    int i,j,k=2;
+    m->cartas[0].palo=4;
+    m->cartas[0].valor=0;
+    m->cartas[1].palo=4;
+    m->cartas[1].valor=0;
+    for(i=0;i<4;i++){
+        for(j=1;j<=12;j++){
+           m->cartas[k].palo=i;
+           m->cartas[k].valor=j;
+           k++;
         }
     }
 }
@@ -31,9 +48,7 @@ void barajar_mazo(Mazo *m){
     srand(time(NULL));
     Carta aux;
     int i,a,b;
-    m->cartas[0]=aux;
-    m->cartas[1]=aux;
-    for(i=0;i<50;i++){
+    for(i=0;i<25;i++){
         a=rand()%50;
         b=rand()%50;
         aux=m->cartas[a];
@@ -48,10 +63,10 @@ Carta devolver_carta(Mazo *m){
 }
 void imprimir_carta(Carta *c){
     switch(c->palo){
-    case 0 : printf("Carta:%d de Basto ",c->valor);
-    case 1 : printf("Carta:%d de Copa ",c->valor);
-    case 2 : printf("Carta:%d de Oro ",c->valor);
-    case 3 : printf("Carta:%d de Espada ",c->valor);
-    case 4 : printf("Carta: Comodín ");
+    case 0 : printf("Carta:%d de Basto\n",c->valor);break;
+    case 1 : printf("Carta:%d de Copa\n",c->valor);break;
+    case 2 : printf("Carta:%d de Oro\n",c->valor);break;
+    case 3 : printf("Carta:%d de Espada\n",c->valor);break;
+    case 4 : printf("Carta: Comodin\n");break;
     }
 }
