36d8fa928d7f6e0e7009111cf5b1ce372d7cdb4d
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-05-09 21:06:24 -0300

    Terminé Ej6 Práctica 4

--- Practica4/Ej6.c
+++ Practica4/Ej6.c
@@ -11,27 +11,67 @@ struct lista
     struct lista *sig;
 };
 typedef struct lista lista;
-void inicializar(lista **l);
-void crear_lista(lista *p,int n);
+
+void inicializar_lista(lista **p,int n);
 void imprimir_lista(lista *p);
+void eliminar_lista(lista **p);
+void agregar_elemeto_principio(lista **p,int e);
+void agregar_elemeto_final(lista **p,int e);
+int cant_elementos(lista *p);
 int main(){
-    lista *p;
-    p->dato=0;
+    /*
+    Si no pongo NULL explicitamente, me reservaba aún así
+    espacio de memoria para una struct lista en todo 0
+    */
+    lista *p=NULL;
     int i;
-    crear_lista(p,5);
+    inicializar_lista(&p,5);
     imprimir_lista(p);
+    agregar_elemeto_principio(&p,0);
+    imprimir_lista(p);
+    agregar_elemeto_final(&p,6);
+    imprimir_lista(p);
+    printf("%d",cant_elementos(p));
 }
 
-void inicializar(lista **l){
-    *l=(lista*) malloc(sizeof(lista));
-}
-void crear_lista(lista *p,int n){
+void inicializar_lista(lista **p,int n){
     int i=0;
     for(i=0;i<n;i++){
-        inicializar(&(p->sig));
-        p->dato=i+1;
+        *p=(lista*) malloc(sizeof(lista));
+        (*p)->dato=i+1;
+        p=&((*p)->sig);
+    }
+}
+void eliminar_lista(lista **p){
+    int i=0;
+    lista *aux;
+    while(*p!=NULL){
+        aux=(*p)->sig;
+        free(*p);
+        *p=aux;
+    }
+}
+void agregar_elemeto_principio(lista **p,int e){
+    lista *aux=(lista*)malloc(sizeof(lista));
+    aux->dato=e;
+    aux->sig=*p;
+    *p=aux;
+}
+void agregar_elemeto_final(lista **p,int e){
+    lista *aux=*p;
+    while(aux->sig!=NULL)aux=aux->sig;
+    aux->sig=(lista*)malloc(sizeof(lista));
+    aux=aux->sig;
+    aux->dato=e;
+    aux->sig=NULL;
+}
+int cant_elementos(lista *p){
+    int cont=0;
+    while(p!=NULL){
+        cont++;
         p=p->sig;
     }
+    return cont;
 }
 void imprimir_lista(lista *p){
     printf("- ");
@@ -39,4 +79,5 @@ void imprimir_lista(lista *p){
         printf("%d - ",p->dato);
         p=p->sig;
     }
+    printf("\n");
 }
