bd135d165330116eb65946676e58ccbc790711d8
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-05-30 21:50:06 -0300

    Terminé Ej7

--- Practica5/Ej7.c
+++ Practica5/Ej7.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 void copiarA(FILE *,FILE *);
+void copiarB(FILE *,FILE *);
+void copiarC(FILE *,FILE *);
 
 int main(){
     FILE *ptrR=fopen("Ej7R.txt","r");
     if(ptrR!=NULL){
         FILE *ptrW=fopen("Ej7W.txt","w");
         if(ptrW!=NULL){
-            copiarA(ptrR,ptrW);
+            copiarB(ptrR,ptrW);
             fclose(ptrR);
         }
         fclose(ptrW);
@@ -22,5 +24,24 @@ void copiarA(FILE *ptrR,FILE *ptrW){
     while(!feof(ptrR)){
         fputc(aux,ptrW);
         aux=fgetc(ptrR);
+    }
+}
+
+void copiarB(FILE *ptrR,FILE *ptrW){
+    char aux[15];
+    fgets(aux,15,ptrR);
+    while(!feof(ptrR)){
+        fputs(aux,ptrW);
+        fgets(aux,15,ptrR);
+    }
+}
+
+void copiarC(FILE *ptrR,FILE *ptrW){
+    char aux[15];
+    int dim;
+    dim = fread(aux,sizeof(char),15,ptrR);
+    while(!feof(ptrR)){
+        fwrite(aux,sizeof(char),dim,ptrW);
+        dim = fread(aux,sizeof(char),15,ptrR);
     }
 }
